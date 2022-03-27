61c837e947147f7feb30fd7386b313e71f5307ed
m21c <miriam.cristofoletti@mbzuai.ac.ae>
2022-03-27 18:14:18 +0400

    add baseline + pre-train

--- ISAID_train_eval.py
+++ ISAID_train_eval.py
@@ -18,12 +18,12 @@ from detectron2.data.datasets import register_coco_instances
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset
 from detectron2.data import build_detection_test_loader
 
-register_coco_instances("iSAID_train", {}, 
-                        "/l/users/miriam.cristofoletti/iSAID/train/instancesonly_filtered_train.json",
-                        "/l/users/miriam.cristofoletti/iSAID/train/images/")
-register_coco_instances("iSAID_val", {}, 
-                        "/l/users/miriam.cristofoletti/iSAID/val/instancesonly_filtered_val.json",
-                        "/l/users/miriam.cristofoletti/iSAID/val/images/")
+register_coco_instances("iSAID_train", {},
+                        "/l/users/miriam.cristofoletti/CV703-project/iSAID/train/instancesonly_filtered_train.json",
+                        "/l/users/miriam.cristofoletti/CV703-project/iSAID/train/images/")
+register_coco_instances("iSAID_val", {},
+                        "/l/users/miriam.cristofoletti/CV703-project/iSAID/val/instancesonly_filtered_val.json",
+                        "/l/users/miriam.cristofoletti/CV703-project/iSAID/val/images/")
                         
 
 cfg = get_cfg()
@@ -35,7 +35,7 @@ cfg.DATALOADER.NUM_WORKERS = 2
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml")  # Let training initialize from model zoo
 cfg.SOLVER.IMS_PER_BATCH = 2
 cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-cfg.SOLVER.MAX_ITER = 100000 
+cfg.SOLVER.MAX_ITER = 5000  #100000 
 cfg.SOLVER.STEPS = []        # do not decay learning rate
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster (default: 512)
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 15  
@@ -43,8 +43,8 @@ cfg.MODEL.ROI_HEADS.NUM_CLASSES = 15
 
 #os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 trainer = DefaultTrainer(cfg)
-trainer.resume_or_load(resume=True)
-#trainer.train()
+trainer.resume_or_load(resume=False)
+trainer.train()
 
 # Inference should use the config with parameters that are used in training
 # cfg now already contains everything we've set previously. We changed it a little bit for inference:
